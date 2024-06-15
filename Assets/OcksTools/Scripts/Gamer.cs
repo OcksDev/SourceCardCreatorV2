4efba85254ac3703458f17c5c35974b6918495d9
OcksDev <148808941+OcksDev@users.noreply.github.com>
2024-06-14 21:47:38 -0400

    sssus
    ss

--- Assets/OcksTools/Scripts/Gamer.cs
+++ Assets/OcksTools/Scripts/Gamer.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Gamer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool[] checks = new bool[20];
     void Start()
     {
-        
+        checks[0] = true;
+        UpdateMenus();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateMenus()
     {
-        
+        Tags.refs["GeneralMenu"].SetActive(checks[0]);
+        Tags.refs["ActionMenu"].SetActive(checks[1]);
+        Tags.refs["GridMenu"].SetActive(checks[2]);
+    }
+
+    public void ToggleMenu(int i)
+    {
+        checks[i] = !checks[i];
+        UpdateMenus();
+    }
+    public void GoToExclusive(int i)
+    {
+        checks[0] = false;
+        checks[1] = false;
+        checks[2] = false;
+        checks[i] = true;
+        UpdateMenus();
     }
 }
---
+++ Assets/OcksTools/Scripts/Misc/Internactct.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Internactct : MonoBehaviour
+{
+    public void UpdateName()
+    {
+        var c = Carder.Instance;
+        var e = GetComponent<TMP_InputField>().text;
+        if (e == "")
+        {
+            c.CurrentCard.data["Name"] = "Unnamed Card";
+        }
+        else
+        {
+            c.CurrentCard.data["Name"] = e;
+        }
+        c.RenderCard(c.CurrentCard);
+    }
+    public void UpdateDesc()
+    {
+        var c = Carder.Instance;
+        var e = GetComponent<TMP_InputField>().text;
+        c.CurrentCard.data["Description"] = e;
+        c.RenderCard(c.CurrentCard);
+    }
+
+}
